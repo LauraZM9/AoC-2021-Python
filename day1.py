f5c7030fac919d9a2fc08ad41247cbcbfc72be76
Laura Zaharia <64781220+LauraZM9@users.noreply.github.com>
2021-12-07 21:06:49 +0000

    Add docstring to functions

--- day1.py
+++ day1.py
@@ -1,22 +1,46 @@
+""" Solution for day 1 from 2021 Advent of Code """
 from utils import read_data
 
 # data is a list of strings
-data = read_data('01')
+data = read_data("01")
 
-def categorize_measurements(measurements: list) -> list:
+
+def categorize_measurements(measurements: list) -> int:
+    """Returns the number of times the measurement increased compared with the previous one.
+
+    Args:
+        measurements (list): list of measurements in the form of strings
+
+    Returns:
+        increased (int): the number of times the measurement increased
+    """
+
     increased = 0
     for i in range(0, len(measurements) - 1):
         if int(measurements[i]) < int(measurements[i + 1]):
             increased += 1
     return increased
 
+
 # print(categorize_measurements(data))
 
-def sliding_window(measurements: list) -> list:
+
+def sliding_window(measurements: list) -> int:
+    """Calculates a sliding window of three measurements and then returns the number of times the measurement increased compared with previous
+
+    Args:
+        measurements (list): list of measurements (strings)
+
+    Returns:
+        increased(int): the number of times the measurement increased
+    """
+
     new_measurements = []
     for i in range(0, len(measurements) - 2):
-        new_measurements.append(int(measurements[i]) + int(measurements[i+1]) + int(measurements[i+2]))
-    increased = categorize_measurements(new_measurements)
-    return increased
+        new_measurements.append(
+            int(measurements[i]) + int(measurements[i + 1]) + int(measurements[i + 2])
+        )
+    return new_measurements
 
-# print(sliding_window(data))
+
+print(categorize_measurements(sliding_window(data)))
