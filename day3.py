e60f4163016b40e986f18b19f22b535fb270f8ae
Laura Zaharia <64781220+LauraZM9@users.noreply.github.com>
2021-12-08 14:08:45 +0000

    Add full solution for day3

--- day3.py
+++ day3.py
@@ -4,29 +4,57 @@ from utils import read_data
 
 data = read_data("03")
 
+def get_most_common_bit(diagnostic:list, position:int) -> str:
+    ones = 0
+    for num in diagnostic:
+        if num[position] == "1":
+            ones += 1
+    if ones >= len(diagnostic)/2:
+        return "1"
+    else:
+        return "0"
+
+def get_least_common_bit(diagnostic:list, position:int) -> str:
+    ones = 0
+    for num in diagnostic:
+        if num[position] == "1":
+            ones += 1
+    if ones >= len(diagnostic)/2:
+        return "0"
+    else:
+        return "1"
+
 def get_power_consumption(diagnostic:list) -> int:
     """Power consumption
     """
     gamma = ""
     epsilon = ""
     for i in range(len(diagnostic[0])):
-        ones = 0
-        for num in diagnostic:
-            if num[i] == "1":
-                ones += 1
-        if ones > len(diagnostic)//2:
-            gamma += "1"
-            epsilon += "0"
-        else:
-            gamma += "0"
-            epsilon += "1"
+        gamma += get_most_common_bit(diagnostic, i)
+        epsilon += get_least_common_bit(diagnostic, i)
     return int(gamma, 2) * int(epsilon, 2)
 
 print(get_power_consumption(data))
 
+def filter_most_common_bit(diagnostic:list) -> str:
+    for i in range(len(diagnostic[0])):
+        diagnostic = [x for x in diagnostic if x[i] == get_most_common_bit(diagnostic, i)]
+        if len(diagnostic) == 1:   
+            break
+    return diagnostic[0]
+
+def filter_least_common_bit(diagnostic:list) -> str:
+    for i in range(len(diagnostic[0])):
+        diagnostic = [x for x in diagnostic if x[i] == get_least_common_bit(diagnostic, i)]
+        if len(diagnostic) == 1:   
+            break
+    return diagnostic[0]
+
 def get_life_support_rating(diagnostic:list) -> int:
     """ Life support rating
     """
-    o2_generator = ""
-    co2_scrubber = ""
-    return 0
+    o2 = filter_most_common_bit(diagnostic)
+    co2 = filter_least_common_bit(diagnostic)
+    return int(o2, 2) * int(co2, 2)
+
+print(get_life_support_rating(data))
