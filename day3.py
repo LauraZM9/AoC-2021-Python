474b177ba017119ebc18af287e11beae3dd00fae
Laura Zaharia <64781220+LauraZM9@users.noreply.github.com>
2021-12-08 00:41:01 +0000

    Add half of solution and test day3

--- day3.py
+++ day3.py
@@ -3,3 +3,30 @@
 from utils import read_data
 
 data = read_data("03")
+
+def get_power_consumption(diagnostic:list) -> int:
+    """Power consumption
+    """
+    gamma = ""
+    epsilon = ""
+    for i in range(len(diagnostic[0])):
+        ones = 0
+        for num in diagnostic:
+            if num[i] == "1":
+                ones += 1
+        if ones > len(diagnostic)//2:
+            gamma += "1"
+            epsilon += "0"
+        else:
+            gamma += "0"
+            epsilon += "1"
+    return int(gamma, 2) * int(epsilon, 2)
+
+print(get_power_consumption(data))
+
+def get_life_support_rating(diagnostic:list) -> int:
+    """ Life support rating
+    """
+    o2_generator = ""
+    co2_scrubber = ""
+    return 0
--- test_all_days.py
+++ test_all_days.py
@@ -1,7 +1,6 @@
-from day1 import categorize_measurements
-from day1 import get_sliding_window
-from day2 import get_final_position
-from day2 import get_correct_position
+from day1 import categorize_measurements, get_sliding_window
+from day2 import get_final_position, get_correct_position
+from day3 import get_life_support_rating, get_power_consumption
 
 
 # Tests day 1
@@ -13,6 +12,7 @@ def test_measurements():
 def test_sling_window():
     assert get_sliding_window(data_to_try), 6
 
+
 # Tests day 2
 instructions = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
 
@@ -22,4 +22,12 @@ def test_position():
 def test_correct_position():
     assert get_correct_position(instructions), 900
 
+
 # Tests day 3
+diagnostic_report = ['00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010']
+
+def test_power_consumption():
+    assert get_power_consumption(diagnostic_report), 198
+
+def test_life_support_rating():
+    assert get_life_support_rating(diagnostic_report), 230
